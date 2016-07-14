e8240d13b00f158a54a852687118410c6d59f495
Tukes <xtukesx@gmail.com>
2016-07-15 00:18:00 +0300

    some fixs for server

--- parser.py
+++ parser.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python 
-# -*- coding: utf-8 -*-
+# -*- coding: UTF-8 -*-
 
 import re
-from pprint import pprint
 
-Input = open('mapKML.kml', 'r')
+#for debuging on local machine
+Input = open('mapKML.kml', 'r', encoding="utf8")
+#for running on server
+#Input = open('mapKML.kml', 'r')
 Output = open('marks.txt', 'w')
 inputString = Input.read()
 pattern = r'<Placemark>(.*?)</Placemark>'
 placemarksList = re.findall(pattern, inputString, flags=re.DOTALL)
 for c in placemarksList:
-    name = re.findall(r'<name>(.*?)</name>', c)[0]
-    if name == '':
+    nameT = re.findall(r'<name>(.*?)</name>', c)
+    if nameT == []:
         name = 'none'
-    description = re.findall(r'<description><!\[CDATA\[(.*?)\]\]></description>', c)
-    if description == []:
+    elif nameT[0] == '':
+        name = 'none'
+    else:
+        name = nameT[0]
+    descriptionT = re.findall(r'<description><!\[CDATA\[(.*?)\]\]></description>', c)
+    if descriptionT == []:
         description = 'none'
     else:
-        description = description[0]
+        description = descriptionT[0]
     coords = re.findall(r'<coordinates>(.*?)</coordinates>', c)[0].split(',')
+    #for debuging on local machine
     print(coords[0] + " - " + coords[1]+ " - " + coords[2]+ " - " + name + " - " + description, file=Output)
+    #for running on server
+    #Output.write(coords[0] + " - " + coords[1]+ " - " + coords[2]+ " - " + name + " - " + description + "\n")
 Input.close()
 Output.close()
