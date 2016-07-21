2dea4113398e8149f633c0086ec9ee8139958dcc
ELynx <esurovin@yandex.ru>
2016-07-21 18:38:00 +0300

    provide option to set up ellipsoid, defaults to WGS-84; more examples

--- stereo.py
+++ stereo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import math
@@ -6,9 +6,9 @@ import math
 def _m_xi_(phi, e):
     sinPhi = math.sin(phi)
     eSinPhi = e * sinPhi
-    
+
     m = math.cos(phi) / math.sqrt(1.0 - eSinPhi * eSinPhi)
-    
+
     xi = (1.0 - eSinPhi) / (1.0 + eSinPhi)
     xi = math.pow(xi, e)
     xi = xi * (1.0 + sinPhi) / (1.0 - sinPhi)
@@ -17,12 +17,21 @@ def _m_xi_(phi, e):
     return (m, xi)
 
 class StereoProjection:
-    def __init__(self, lat0, lon0): #shirota-dolgota phi-lambda N&E - positive, S&W - negative
+    #shirota-dolgota phi-lambda N&E - positive, S&W - negative
+    def __init__(self, lat0, lon0, a = None, e = None, k0 = None):
         self.lat0 = math.radians(lat0)
         self.lon0 = math.radians(lon0)
-        self.a = 6378206.4
-        self.e = 0.0822719
-        self.k0 = 0.9999
+
+        #defaul is WGS-84
+        if a == None:
+            self.a = 6378137.0
+            b = 6356752.314140
+            self.e = math.sqrt(1 - b * b / self.a / self.a)
+            self.k0 = 1.0
+        else:
+            self.a = a
+            self.e = e
+            self.k0 = k0
 
         self.m0, xi0 = _m_xi_(self.lat0, self.e)
 
@@ -37,7 +46,7 @@ class StereoProjection:
 
         sinXi1 = math.sin(xi1)
         cosXi1 = math.cos(xi1)
-        
+
         dLambda = lon1 - self.lon0
         sinDLambda = math.sin(dLambda)
         cosDLambda = math.cos(dLambda)
@@ -49,12 +58,27 @@ class StereoProjection:
         k = A * cosXi1 / self.a / m1
 
         return (x, y, k)
-        
+
 
 if __name__ == "__main__":
-    stereo = StereoProjection(40.0, -100.0)
+    #do the book numeric examples
+    print("Example on page 313")
+    stereo = StereoProjection(40.0, -100.0, 6378206.4, 0.0822719, 0.9999)
     x, y, k = stereo.geoToStereo(30.0, -90.0)
     print(str(x))
     print(str(y))
     print(str(k))
+    print("Book gives 971630.8 -1063049.3 1.0121248")
+
+    #Saint-Pete with WGS-84
+    print("Find distance to Rif fort")
+    stereo = StereoProjection(59.938630, 30.314130)
+    #around Rif fort
+    x, y, k = stereo.geoToStereo(60.033175, 29.638552)
+    print(str(x))
+    print(str(y))
+    print(str(k))
+    distance = math.sqrt(x * x + y * y)
+    print(str(distance / 1000.0) + "km")
+    print("Should be around 39 km")
 
