7c65e06237e284e2af1011012247e5b8fd0a2b7c
ELynx <esurovin@yandex.ru>
2016-07-21 17:08:45 +0300

    working example

--- stereo.py
+++ stereo.py
@@ -4,15 +4,17 @@
 import math
 
 def mapDegreeToRad(degreeString):
-    parts = degreeString.split("-")
+    parts = degreeString.split(".")
     degree = float(parts[0]) + (float(parts[1]) / 60.0) + (float(parts[2]) / 360.0)
     return math.radians(degree)
 
 def xi(phi, e):
     sinPhi = math.sin(phi)
     eSinPhi = e * sinPhi
-    result = ((1.0 + sinPhi)/(1.0 - sinPhi))*((1.0 - eSinPhi)/(1.0 + eSinPhi))
-    result = math.pow(result, e / 2.0)
+    result = (1.0 - eSinPhi)/(1.0 + eSinPhi)
+    result = math.pow(result, e)
+    result = ((1.0 + sinPhi)/(1.0 - sinPhi)) * result
+    result = math.sqrt(result)
     result = 2.0 * math.atan(result)
     result = result - (math.pi / 2.0)
     return result
@@ -22,11 +24,8 @@ def m(phi, e):
     result = (math.cos(phi))/(math.pow(1.0 - (eSinPhi * eSinPhi), 0.5))
     return result
 
-def printRadians(rad):
-    print(str(math.degrees(rad)))
-
 class StereoProjection:
-    def __init__(self, lat0Str, lon0Str): #shirota-dolgota phi-lambda
+    def __init__(self, lat0Str, lon0Str): #shirota-dolgota phi-lambda N&E - positive, S&W - negative
         self.lat0 = mapDegreeToRad(lat0Str)
         self.lon0 = mapDegreeToRad(lon0Str)
 
@@ -51,14 +50,9 @@ class StereoProjection:
         m0 = m(self.lat0, e)
         m1 = m(lat1, e)
 
-        printRadians(xi1)
-        printRadians(xi0)
-        print(str(m1))
-        print(str(m0))
-
-        cosXi1 = math.cos(xi1)
         sinXi1 = math.sin(xi1)
-
+        cosXi1 = math.cos(xi1)
+        
         sinXi0 = math.sin(xi0)
         cosXi0 = math.cos(xi0)
         
@@ -66,22 +60,18 @@ class StereoProjection:
         sinDLambda = math.sin(dLambda)
         cosDLambda = math.cos(dLambda)
 
-        A = (2.0 * a * k0 * m1)/(cosXi1 * (1.0 - sinXi1 * sinXi0 + cosXi1 * cosXi0 * cosDLambda))
+        A = (2.0 * a * k0 * m0)/(cosXi0 * (1.0 + sinXi0 * sinXi1 + cosXi0 * cosXi1 * cosDLambda))
 
-        x = A * cosXi0 * sinDLambda
-        y = A * (cosXi1 * sinXi0 - sinXi1 * cosXi0 * cosDLambda)
-        k = (A * cosXi0)/(a * m0)
-        
-        print(str(A))
+        x = A * cosXi1 * sinDLambda
+        y = A * (cosXi0 * sinXi1 - sinXi0 * cosXi1 * cosDLambda)
+        k = (A * cosXi1)/(a * m1)
 
         return (x, y, k)
         
 
 if __name__ == "__main__":
-    stereo = StereoProjection("59-56-00", "30-19-00") #TODO define simple format for deg-min-sec
-    print(str(stereo.lat0))
-    print(str(stereo.lon0))
-    x, y, k = stereo.geoToStereo("30-00-00", "90-00-00")
+    stereo = StereoProjection("40.00.00", "-100.00.00") #TODO define simple format for deg-min-sec
+    x, y, k = stereo.geoToStereo("30.00.00", "-90.00.00")
     print(str(x))
     print(str(y))
     print(str(k))
